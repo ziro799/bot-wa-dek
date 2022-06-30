4f22f9fe731fd1ba8e757ac27714cbc1c1af7426
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-30 10:53:21 +0800

    ArullOfc

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,6 +1,6 @@
+let fetch = require('node-fetch')
 let PhoneNumber = require('awesome-phonenumber')
-let levelling = require('../lib/levelling')
-let handler = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn }) => {
   let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
@@ -11,45 +11,24 @@ let handler = async (m, { conn, usedPrefix }) => {
     let { name, premium, level, limit, exp, lastclaim, registered, regTime, age } = global.DATABASE.data.users[m.sender]
     let username = conn.getName(who)
     let str = `
-        exp: 0,
-        limit: 10,
-        lastclaim: 0,
-        registered: false,
-        name: 'Belum Daftar:v',
-        age: -1,
-        regTime: -1,
-        afk: -1,
-        afkReason: '',
-        banned: false,
-        level: 0,
-        call: 0,
-        role: 'Warrior V',
-        autolevelup: false,
-        pc: 0,
-      }
-    }
-    let { name, limit, exp, lastclaim, registered, regTime, age, level, role, banned } = global.db.data.users[who]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let username = conn.getName(who)
-    let math = max - xp
-    let res = `http://hardianto-chan.herokuapp.com/api/rankcard?profile=https://i.ibb.co/vQTHzkh/IMG-20210907-WA0721.jpg&name=${name}&bg=https://i.ibb.co/4YBNyvP/images-76.jpg&needxp=${max}&curxp=${exp}&level=${level}&logorank=https://i.ibb.co/Wn9cvnv/FABLED.png`
-    let str = `
-Nama: ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
-Nomor: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}${registered ? '\nUmur: ' + age : ''}
-XP: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Siap untuk *${usedPrefix}levelup*` : `${math} XP lagi untuk levelup`}]
-Level: ${level}
-Role: *${role}*
-Limit: ${limit}
-Owner: ${global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) ? 'Ya' : 'Tidak'}
-Premium: ${global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) ? 'Ya' : 'Tidak'}
-Terdaftar: ${registered ? 'Ya (' + new Date(regTime).toLocaleString() + ')' : 'Tidak'}${lastclaim > 0 ? '\nTerakhir Klaim: ' + new Date(lastclaim).toLocaleString() : ''}
+
+✧───────[ *PROFILE* ]───────✧
+📇 • *Name:* ${username} ${registered ? '(' + name + ') ': ''}
+📧 • *Tag:* @${who.replace(/@.+/, '')}
+📞 • *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+💻 • *Link:* https://wa.me/${who.split`@`[0]}
+${registered ? '🎨 • *Age:* ' + age : ''}
+
+🌟 • *Premium:* ${premium ? "✅" :"❌"}
+📑 • *Registered:* ${registered ? '✅': '❌'}
+⛔ • *Banned:* ❌
+
 `.trim()
     let mentionedJid = [who]
-    conn.sendFile(m.chat, pp, 'pp.jpg', banned ? 'jiakh ke banned' : str, m, false, { contextInfo: { mentionedJid } })
+    conn.sendFile(m.chat, await(await require('node-fetch')(pp)).buffer(), pp.jpg, str, m, false, { contextInfo: { mentionedJid }})
   }
 }
 handler.help = ['profile [@user]']
 handler.tags = ['tools']
-handler.command = /^profile?$/i
+handler.command = /^profile|pp$/i
 module.exports = handler
