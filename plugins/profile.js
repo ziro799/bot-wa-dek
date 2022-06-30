82f4656250275de3369cdb99713ff5dd364a5939
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-30 10:45:18 +0800

    ARULL OFC

--- plugins/profile.js
+++ plugins/profile.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix }) => {
   let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
-    pp = await conn.getProfilePicture(who)
+    pp = await conn.profilePictureUrl(who, 'image')
   } catch (e) {
 
   } finally {
